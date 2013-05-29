74364cd6b32cb475e3e40714caab966a25a0064f
Aimee Maher <aimee@geekycheeks.com>
2013-05-29 13:58:45 -0600

    added readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheHunt::Application.config.secret_token = '3265ccd96b551d3ba13aee4abfdc5550a374520792469a5e547b9998437d06b30435c22435031ed6d64ed9649b48fb3b7606dff80e566793e22276e6b9267150'
