5223cd5b2ac88f15389cfc9be5e9e0d957d349f3
Aimee Maher <aimee@geekycheeks.com>
2013-05-29 23:16:41 -0600

    Added Home, Users and Sessions controllers and models to start working on integrating omniauth-foursquare authentication.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :foursquare, "R4G5CER4VUSPJVJZSSF5R0RDGFQINY5IWNI2YOFIKCEOPYUX", 
+  "MNWEDCFXRKSSKFH4H1DKS4GCVTQU5KWSRD3EYWS24MP0VBK4" 
+end
