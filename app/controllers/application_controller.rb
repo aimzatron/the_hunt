a2b21406cfc4f6287f07eb416a630dabad0c69da
Aimee Maher <aimee@geekycheeks.com>
2013-06-03 23:44:33 -0600

    finished front page javascript animation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,23 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def find_user_name
-    current_id = current_user.uid
+  def client
     client = Foursquare2::Client.new(:client_id => 'R4G5CER4VUSPJVJZSSF5R0RDGFQINY5IWNI2YOFIKCEOPYUX', :client_secret => 'MNWEDCFXRKSSKFH4H1DKS4GCVTQU5KWSRD3EYWS24MP0VBK4')
+  end
+
+  def current_id
+    current_user.uid
+  end
+
+  def find_user_name
     client.user(current_id).firstName
   end
   helper_method :find_user_name
+
+  def find_user_photo
+    client.user(current_id).photo
+  end
+  helper_method :find_user_photo
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
