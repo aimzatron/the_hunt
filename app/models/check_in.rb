fc11c431838c7e316f55e95b2f4eb5688b9cec63
Aimee Maher <aimee@geekycheeks.com>
2013-06-18 10:31:34 -0600

    Deleted Rails generated test files, helper files, multi_game and solo_game models as they are not in use.

--- app/models/check_in.rb
+++
@@ -1,7 +0,0 @@
-class CheckIn < ActiveRecord::Base
-  include ApplicationHelper
-  belongs_to :user
-  attr_accessible :venue, :text, :check_in_id, :checkin_time, 
-                  :address, :latitude, :longitude
-
-end
--- app/models/solo_game.rb
+++
@@ -1,4 +0,0 @@
-class SoloGame < Game
-
-
-end
